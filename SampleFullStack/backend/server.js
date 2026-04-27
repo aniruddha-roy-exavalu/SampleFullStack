cdac372700130f1c72925e1de2a66814b0205e24
aniruddha-roy-exavalu <aniruddha.roy@exavalu.com>
2026-04-27 22:38:32 +0530

    Add files via upload

---
+++ SampleFullStack/backend/server.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.get('/api/hello',(req,res)=>{
+  res.json({message:'Hello from Node backend 🚀'});
+});
+app.listen(5000,()=>console.log('Backend running on http://localhost:5000'));
