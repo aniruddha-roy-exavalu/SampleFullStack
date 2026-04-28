d516bb3c3ccbc9a8238e39769eea96f493443613
aniruddha-roy-exavalu <aniruddha.roy@exavalu.com>
2026-04-28 12:42:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
-
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
+const port = process.env.PORT || 4000;
+
 app.use(cors());
-app.get('/api/hello',(req,res)=>{
-  res.json({message:'Hello from Node backend 🚀'});
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Node backend 🚀' });
 });
-app.listen(5000,()=>console.log('Backend running on http://localhost:5000'));
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Backend running on port ${port}`);
+});
